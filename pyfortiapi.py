480f1441fb0f8e5b728eb40be813bd544339e082
James <james.simpson2@education.wa.edu.au>
2017-09-07 18:55:49 +0800

    updated pyfortiapi.py

--- pyfortiapi.py
+++ pyfortiapi.py
@@ -4,3 +4,31 @@
 import requests
 import json
 
+# Disable requests' warnings for insecure connections
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+class FortiGate():
+    def __init__(self, hostname, ipaddr, username, password):
+
+        self.hostname = hostname
+        self.ipaddr = ipaddr
+        self.username = username
+        self.password = password
+
+    def roll_session(self):
+        session = requests.session()
+        url = f'https://{self.hostname}/logincheck'
+
+        # Login
+        session.post(url, data=f'username={self.username}&secretkey={self.password}', verify=False, timeout=10)
+
+        # Get CSRF token from cookies, add to headers
+        for cookie in session.cookies:
+            if cookie.name == 'ccsrftoken':
+                csrftoken = cookie.value[1:-1]  # strip quotes
+                session.headers.update({'X-CSRFTOKEN': csrftoken})
+        return session
+
