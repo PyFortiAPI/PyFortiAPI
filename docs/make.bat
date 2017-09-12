6c97095388e08f5d0ee57b78707b13363633b163
James <james.simpson2@education.wa.edu.au>
2017-09-12 09:41:46 +0800

    Docs

--- docs/make.bat
+++ docs/make.bat
@@ -7,9 +7,9 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=python -msphinx
 )
-set SOURCEDIR=source
-set BUILDDIR=build
-set SPHINXPROJ=Forti-Api
+set SOURCEDIR=.
+set BUILDDIR=_build
+set SPHINXPROJ=PyFortiAPI
 
 if "%1" == "" goto help
 
